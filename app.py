f12df4c9e4b3887bbb8f3bded9b3c3bc41c14539
kkavitha <kkavitha@thoughtworks.com>
2019-02-18 15:26:02 +0530

    Add templates for UI

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, request
+import psycopg2
+from flask import Flask, request, render_template
 from flask_cors import CORS
+
+from modules.post_data_to_database import post_data
 
 app = Flask(__name__)
 CORS(app)
+connection = psycopg2.connect("dbname=cafeteria user=postgres")
 
 
 @app.route('/')
 def home():
-    return "Hello world"
+    return render_template('homepage.html')
 
 
 @app.route('/post-data', methods=['POST'])
 def get_data():
-    print(request)
-    return "Hello world"
+    post_data(connection, request.form)
+    return render_template('detailspage.html', shared=request.form)
 
 
 if __name__ == '__main__':
